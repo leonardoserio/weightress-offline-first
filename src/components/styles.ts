cbcb0ea68b0ff68307df9e2d8c6d8bf1e3401fcc
Leonardo Nascimento <leonardo.nascimento@cbyk.com.br>
2021-03-16 18:22:42 -0300

    create styles

---
+++ src/components/styles.ts
@@ -0,0 +1,72 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+export const primaryColor = '#FB8C00';
+
+export const headerStyles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    justifyContent: 'space-between',
+  },
+  addButton: {
+    borderColor: primaryColor,
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 3,
+    borderWidth: 1,
+  },
+  headerTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    borderLeftWidth: 3,
+    paddingLeft: 10,
+    borderLeftColor: primaryColor,
+  },
+});
+
+const windowDim = Dimensions.get('window');
+export const windowHeight = windowDim.height;
+export const creatorStyles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+  },
+  modalView: {
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 10,
+    height: windowHeight / 2,
+    shadowColor: '#cacaca',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  topActions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  topCloseButton: {
+    padding: 10,
+    color: '#494949',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 3,
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: '#c9c9c9',
+  },
+});
